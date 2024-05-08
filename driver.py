3fc7b8004f3db81e565a2825aaf3919d49d489fc
Diana Sanchez Ordoñez <dianasanchezordonez@Dianas-MacBook-Pro.local>
2024-05-08 14:15:00 -0500

    chore: add message rule on fc_questions_rules

--- driver.py
+++ driver.py
@@ -6,14 +6,13 @@ engine = knowledge_engine.engine(__file__)
 def fc_rules_fn():
   engine.reset()
   engine.activate("fc_rules")
-  print("doing proof")
 
   try:
-    with engine.prove_goal("facts.has_depression($name_patient, $status)") as gen:
-      for vars, plan in gen:
-        name_patient = vars['name_patient']
-        status = vars['status']
-        print("Dear", name_patient, "-", status)
+    # with engine.prove_goal("facts.has_depression($name_patient, $status)") as gen:
+    #   for vars, plan in gen:
+    #     name_patient = vars['name_patient']
+    #     status = vars['status']
+    #     print("Dear", name_patient, "-", status)
 
     with engine.prove_goal("facts.recommendation_message($name_patient, $message)") as gen:
       for vars, plan in gen:
@@ -28,19 +27,23 @@ def fc_rules_fn():
 def fc_questions_fn():
   engine.reset()
   engine.activate("fc_questions_rules")
-  print("doing proof")
 
   try:
-    with engine.prove_goal("facts.depression_severity($total_score, $severity)") as gen:
+    # with engine.prove_goal("facts.depression_severity($total_score, $severity)") as gen:
+    #   for vars, plan in gen:
+    #     name_patient = vars['total_score']
+    #     total_score = vars['severity']
+    #     print("Dear", name_patient, "-", total_score)
+
+    with engine.prove_goal("facts.recommendation_message($name_patient, $message)") as gen:
       for vars, plan in gen:
-        # Accede a las variables de la regla correctamente
-        name_patient = vars['total_score']
-        total_score = vars['severity']
-        print("Dear", name_patient, "-", total_score)
+        print("Dear %s," % vars['name_patient'].capitalize())
+        print(vars['message'])
   except Exception as e:
     print("Error:", e)
     krb_traceback.print_exc()
     sys.exit(1)
 
 if __name__ == "__main__":
-    fc_rules_fn()
+  # fc_rules_fn()
+  fc_questions_fn()
