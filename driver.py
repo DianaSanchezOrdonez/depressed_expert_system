3a70ea0f0e0f417f1e19d5a9282c8aa3ce9c858a
Diana Sanchez Ordoñez <dianasanchezordonez@Dianas-MacBook-Pro.local>
2024-05-09 12:06:05 -0500

    chore: improve format message output

--- driver.py
+++ driver.py
@@ -8,17 +8,13 @@ def fc_rules_fn():
   engine.activate("fc_rules")
 
   try:
-    # with engine.prove_goal("facts.has_depression($name_patient, $status)") as gen:
-    #   for vars, plan in gen:
-    #     name_patient = vars['name_patient']
-    #     status = vars['status']
-    #     print("Dear", name_patient, "-", status)
-
-    with engine.prove_goal("facts.recommendation_message($name_patient, $message)") as gen:
+    with engine.prove_goal("facts.recommendation_message($name_patient, $age, $gender, $message)") as gen:
       for vars, plan in gen:
-        name_patient = vars['name_patient']
-        message = vars['message']
-        print("Dear", name_patient, "-", message)
+        print("\n   Nombre del Paciente: %s" % (vars['name_patient']).capitalize())
+        print("------------------------------------")
+        print("    Edad: %s" % (vars['age']))
+        print("    Género: %s" % (vars['gender']).capitalize())
+        print("    Recomendación: %s \n" % (vars['message']))
   except Exception as e:
     print("Error:", e)
     krb_traceback.print_exc()
@@ -29,12 +25,6 @@ def fc_questions_fn():
   engine.activate("fc_questions_rules")
 
   try:
-    # with engine.prove_goal("facts.depression_severity($total_score, $severity)") as gen:
-    #   for vars, plan in gen:
-    #     name_patient = vars['total_score']
-    #     total_score = vars['severity']
-    #     print("Dear", name_patient, "-", total_score)
-
     with engine.prove_goal("facts.recommendation_message($name_patient, $message)") as gen:
       for vars, plan in gen:
         print("Dear %s," % vars['name_patient'].capitalize())
@@ -45,5 +35,5 @@ def fc_questions_fn():
     sys.exit(1)
 
 if __name__ == "__main__":
-  # fc_rules_fn()
-  fc_questions_fn()
+  fc_rules_fn()
+  # fc_questions_fn()
